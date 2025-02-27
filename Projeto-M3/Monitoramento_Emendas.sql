a16863d3f797f71720d9c66bdcfc0a06d21e6004
Guilherme-lgs <melloguilerme38@gmail.com>
2025-02-27 19:27:31 -0300

    ✨ add new table empresa

--- Projeto-M3/Monitoramento_Emendas.sql
+++ Projeto-M3/Monitoramento_Emendas.sql
@@ -9,3 +9,11 @@ CREATE TABLE Politico (
     cargo VARCHAR(100) NOT NULL,
     estado CHAR(2) NOT NULL
 );
+
+CREATE TABLE Empresa (
+    cnpj id INT PRIMARY KEY NOT NULL,
+    razao_social VARCHAR(200) NOT NULL,
+    nome_fantasia VARCHAR(200) NOT NULL,
+    setor VARCHAR(100) NOT NULL,
+    localizacao VARCHAR(100) NOT NULL
+    );
